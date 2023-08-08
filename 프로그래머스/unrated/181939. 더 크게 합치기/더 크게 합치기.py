5653d56a2d26b4a038000652e104955020991dab
UHa <141496181+YouHaaaa@users.noreply.github.com>
2023-08-08 11:33:00 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,3 +1,4 @@
 def solution(a, b):
     a, b = str(a), str(b)
-    return int(a + b) if int(a + b) > int(b + a) else int(b + a)
+    # return int(a + b) if int(a + b) > int(b + a) else int(b + a
+    return max(int(a + b), int(b + a))
