ba5616459ba39938cab72fa3c03ae8deefa8a315
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-12 17:00:48 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,9 +1,16 @@
 def solution(n, arr = []): 
-    arr.append(n);
-    if n == 1:
-        return arr;
-    if n % 2 == 0:
-        return solution(n / 2, arr)
-    return solution(3 * n + 1, arr)
+    # arr.append(n);
+    # if n == 1:
+    #     return arr;
+    # if n % 2 == 0:
+    #     return solution(n / 2, arr)
+    # return solution(3 * n + 1, arr)
     
-    
+    result = [n]
+    while n > 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        result.append(n)
+    return result
