ddbd91e04bf1b47c8d3de16a5ddab77739e6d0c2
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-14 02:12:33 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,5 +1,9 @@
 function solution(x1, x2, x3, x4) {
-    const x1x2 = x1 === false && x2 === false ? false : true;
-    const x3x4 = x3 === false && x4 === false ? false : true;
-    return x1x2 === true && x3x4 === true
+    //ver1
+    // const x1x2 = x1 === false && x2 === false ? false : true;
+    // const x3x4 = x3 === false && x4 === false ? false : true;
+    // return x1x2 === true && x3x4 === true
+    
+    //ver2
+    return (x1 || x2) && (x3 || x4)
 }
