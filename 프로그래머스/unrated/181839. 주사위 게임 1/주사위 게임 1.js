c9caafbc512502b90312e85cdabef61c9bd895d5
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-17 16:36:56 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,5 +1,11 @@
 function solution(a, b) {
-    if(a % 2 === 1 && b % 2 === 1) return a**2 + b**2
-    else if(a % 2 === 1 || b % 2 === 1) return 2 * (a + b)
-    else return Math.abs(a - b)
+    // if(a % 2 === 1 && b % 2 === 1) return a**2 + b**2
+    // else if(a % 2 === 1 || b % 2 === 1) return 2 * (a + b)
+    // else return Math.abs(a - b)
+    
+    const isOdd = (num) => {
+        return num % 2 === 1;
+    }
+    
+    return isOdd(a) && isOdd(b) ? a**2 + b**2 : isOdd(a) || isOdd(b) ? 2 * (a + b) : Math.abs(a - b);
 }
