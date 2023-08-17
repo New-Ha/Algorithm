d2f9bd50d3d426fbaaab3ab979f1116aef3f05d3
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-17 16:52:49 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,2 +1,5 @@
 def solution(myString, pat):
     return int(pat in ''.join(map(lambda el : 'B' if el == 'A' else 'A', list(myString))))
+
+    # pat을 반전시켜 찾기
+    # return int(''.join(['A' if i == 'B' else 'B' for i in pat]) in myString)
