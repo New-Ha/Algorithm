b82ecfe12a44c7c9bb73dd9ca3eb556967aad0f6
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 02:57:26 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,2 +1,6 @@
 def solution(n):
+    # numList = list(map(int, list(str(n))));
+    # strList = list(map(str, sorted(numList, reverse = True)));
+    # return int("".join(strList))
+
     return int(''.join(sorted(list(map(str,list(map(int,list(str(n)))))), reverse = True)))
