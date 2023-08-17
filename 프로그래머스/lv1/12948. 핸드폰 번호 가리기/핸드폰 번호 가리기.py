9ce5fb7d4594b8a29c052f77b818e42781b532bc
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 03:37:35 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,4 +1,6 @@
-import numpy as np
+# import numpy as np
 
 def solution(phone_number):
-    return ''.join(np.repeat('*', len(phone_number) - 4)) + phone_number[-4:]   
+    # return ''.join(np.repeat('*', len(phone_number) - 4)) + phone_number[-4:]   
+    
+    return '*' * (len(phone_number) - 4) + phone_number[-4:]   
