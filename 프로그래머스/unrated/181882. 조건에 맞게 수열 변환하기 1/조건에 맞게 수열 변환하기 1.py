124e5904286165073a7c7353039d8280490964b2
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-17 17:01:44 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 10.24 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,2 +1,4 @@
 def solution(arr):
-    return list(map(lambda el : el / 2 if el >= 50 and el % 2 == 0 else el * 2 if el < 50 and el % 2 == 1 else el, arr))
+    # return list(map(lambda el : el / 2 if el >= 50 and el % 2 == 0 else el * 2 if el < 50 and el % 2 == 1 else el, arr))
+
+    return [n / 2 if n >= 50 and n % 2 == 0 else n * 2 if n < 50 and n % 2 == 1 else n for n in arr]
