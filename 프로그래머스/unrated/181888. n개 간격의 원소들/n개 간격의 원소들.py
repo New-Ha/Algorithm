5edef0c3786fa12e637bda464809a81565c2d21c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-17 17:07:29 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,6 +1,13 @@
 def solution(num_list, n):
-    result = [];
-    for i in range(len(num_list)):
-        if i % n == 0:
-            result.append(num_list[i]);
-    return result;
+    # ver 1
+    # result = [];
+    # for i in range(len(num_list)):
+    #     if i % n == 0:
+    #         result.append(num_list[i]);
+    # return result;
+
+    # ver 2
+    # return [num_list[i] for i in range(0,len(num_list),n)]
+
+    #ver 3
+    return num_list[::n]
