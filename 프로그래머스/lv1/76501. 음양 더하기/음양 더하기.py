131707f99fac9096621917da2bb75e68d90d562d
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 03:26:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,12 +1,12 @@
 def solution(absolutes, signs):
     # ver 1
-    # result = 0;
-    # for i in range(len(signs)):
-    #     if signs[i]:
-    #         result += absolutes[i];
-    #     else:
-    #         result -= absolutes[i];
-    # return result
+    result = 0;
+    for i in range(len(signs)):
+        if signs[i]:
+            result += absolutes[i];
+        else:
+            result -= absolutes[i];
+    return result
     
     # ver 2
-    return sum(absolutes[i] * 1 if signs[i] else absolutes[i] * -1 for i in range(len(signs)))
+    # return sum(absolutes[i] * 1 if signs[i] else absolutes[i] * -1 for i in range(len(signs)))
