3ddf763777c802e75d8b130fd924d6564694537c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 02:42:38 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,5 +1,8 @@
-import math
+# import math
 
 def solution(n):
-    x = math.sqrt(n);
-    return (x + 1)**2 if x == int(x) else -1 ;
+    # x = math.sqrt(n);
+    # return (x + 1)**2 if x == int(x) else -1 ;
+    
+    x = n**0.5;
+    return (x + 1)**2 if x == int(x) else -1;
