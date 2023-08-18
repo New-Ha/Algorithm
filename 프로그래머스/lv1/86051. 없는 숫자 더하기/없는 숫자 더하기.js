f58cbe052b0570516f34b851725915a2ddc02be6
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 16:38:07 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,4 +1,6 @@
 function solution(numbers) {
-    const zeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    return zeroToNine.filter(el => !numbers.includes(el)).reduce((acc, cur) => acc + cur, 0)
+//     const zeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//     return zeroToNine.filter(el => !numbers.includes(el)).reduce((acc, cur) => acc + cur, 0)
+    
+    return 45 - numbers.reduce((acc, cur) => acc + cur, 0)
 }
