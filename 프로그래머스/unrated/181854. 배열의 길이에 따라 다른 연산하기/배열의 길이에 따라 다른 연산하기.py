14462b398862fb0151b3d612137a5ce579ba247b
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 22:18:22 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -4,3 +4,10 @@ def solution(arr, n):
         return list(map(lambda x, i : x + n if i % 2 == 0 else x, arr, idx));
     else:
         return list(map(lambda x, i : x + n if i % 2 == 1 else x, arr, idx));
+    
+    # N=len(arr)
+    # if N%2:
+    #     for i in range(0,N,2): arr[i]+=n
+    # else:
+    #     for i in range(1,N,2): arr[i]+=n
+    # return arr
