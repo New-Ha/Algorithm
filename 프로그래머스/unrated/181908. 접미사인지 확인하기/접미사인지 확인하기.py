34781e183137b8ae2187694d9729f491abe489bb
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-18 22:27:40 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -2,7 +2,7 @@ def solution(my_string, is_suffix):
     suffix = [my_string[i:] for i in range(len(my_string))]
     return +(is_suffix in suffix)
 
-    # if m[-len(s):]==s: return 1
+    # if my_string[-len(is_suffix):] == is_suffix: return 1
     # return 0
     
     # return int(my_string.endswith(is_suffix))
