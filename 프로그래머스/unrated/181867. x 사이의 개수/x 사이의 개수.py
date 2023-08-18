9ef609154cb43eae5a8e7b2faa8805476115085d
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-19 00:12:11 +0900

    [unrated] Title: x 사이의 개수, Time: 1.55 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,2 +1,4 @@
 def solution(myString):
-    return list(map(lambda el: len(el), myString.split('x')))
+    # return list(map(lambda el: len(el), myString.split('x')))
+    
+    return [len(x) for x in myString.split('x')]
