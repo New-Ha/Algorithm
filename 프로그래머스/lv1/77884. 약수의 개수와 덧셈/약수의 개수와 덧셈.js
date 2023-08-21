1055156afa6b80023b41e29047d788983d50e573
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-21 13:01:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,26 @@
-function boolDivisor(n) {
-    let count = 0;
-    for(let i = 1; i <= n; i++){
-        if(n % i === 0) count++
-    }
-    return count % 2 == 0 ? 1 : -1
-}
+// function boolDivisor(n) {
+//     let count = 0;
+//     for(let i = 1; i <= n; i++){
+//         if(n % i === 0) count++
+//     }
+//     return count % 2 == 0 ? 1 : -1
+// }
 
 function solution(left, right) {
-    const rangeNum = [];
+    // const rangeNum = [];
+    // for(let i = left; i <= right; i++){
+    //     rangeNum.push(i)
+    // }
+    // return rangeNum.reduce((acc, cur) => acc + (cur * boolDivisor(cur)), 0)
+    
+    let result = 0;
     for(let i = left; i <= right; i++){
-        rangeNum.push(i)
+        let count = 0;
+        for(let j = 1; j <= i; j++){
+            if(i % j === 0) count++
+        }
+        if(count % 2 === 0) result += i
+        else result -= i
     }
-    const divisorResult = [...rangeNum].map(el => boolDivisor(el) )
-    return rangeNum.reduce((acc, cur, idx) => acc + (cur * divisorResult[idx]), 0)
+    return result
 }
