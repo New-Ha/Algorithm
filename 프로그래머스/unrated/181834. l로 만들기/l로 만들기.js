32a6043e1fcfb8546c6d6881ad04fb72a047455d
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-21 11:50:48 +0900

    [unrated] Title: l로 만들기, Time: 11.65 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,4 +1,6 @@
 function solution(myString) {
-    const goL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
-    return myString.split('').map(el => goL.includes(el) ? 'l' : el).join('')
+    // const goL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'];
+    // return myString.split('').map(el => goL.includes(el) ? 'l' : el).join('')
+    
+    return myString.split('').map(el => el < 'l' ? 'l' : el).join('')
 }
