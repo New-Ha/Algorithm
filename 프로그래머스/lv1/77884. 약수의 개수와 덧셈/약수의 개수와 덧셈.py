35f6fa09208de7d2d685333f6e798c4875f7c323
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-21 13:12:05 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,9 +1,17 @@
 def solution(left, right):
-    result = 0;
-    for i in range(left, right + 1):
-        count = 1;
-        for j in range (2, i + 1):
-            if i % j == 0: count += 1;
-        if count % 2 == 0: result += i;
-        else: result -= i;
-    return result
+    # result = 0;
+    # for i in range(left, right + 1):
+    #     count = 1;
+    #     for j in range (2, i + 1):
+    #         if i % j == 0: count += 1;
+    #     if count % 2 == 0: result += i;
+    #     else: result -= i;
+    # return result
+    
+    def calcDivisor(n):
+        count = 0;
+        for i in range(1, n + 1):
+            if n % i == 0: count += 1;
+        return n if count % 2 == 0 else -n
+    
+    return sum(list(map(calcDivisor, [x for x in range(left, right + 1)])))
