6b7f4980041b93ae5294ca574fad081e1968158f
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-22 16:05:13 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,8 +1,10 @@
 function solution(date1, date2) {
-    const [y1, m1, d1] = date1;
-    const [y2, m2, d2] = date2;
+//     const [y1, m1, d1] = date1;
+//     const [y2, m2, d2] = date2;
     
-    // return +(y1 < y2 || (y1 === y2 && m1 < m2) || (y1 === y2 && m1 === m2 && d1 < d2))
+//     return +(y1 < y2 || (y1 === y2 && m1 < m2) || (y1 === y2 && m1 === m2 && d1 < d2))
     
     return +(date1.join('') - date2.join('') < 0);
+    
+    // return +(new Date(date1) < new Date(date2))
 }
