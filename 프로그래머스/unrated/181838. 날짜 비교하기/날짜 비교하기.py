36f887682b11d11b7eec4b06c38d416669467907
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-22 16:13:59 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,4 +1,3 @@
 def solution(date1, date2):
-    date1Sum = str(date1[0])+str(date1[1])+str(date1[2]);
-    date2Sum = str(date2[0])+str(date2[1])+str(date2[2]);
-    return 1 if int(date1Sum) - int(date2Sum) < 0 else 0
+    return int(int(''.join(str(i) for i in date1)) < int(''.join(str(i) for i in date2))) 
+    # return int(date1 < date2)
