dd2437d4b11efb60e6def2ed4182afc2736a512a
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-23 17:28:39 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 3.74 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,2 +1,4 @@
 def solution(myString):
-    return list(filter(str, sorted(myString.split('x'))))
+    # return list(filter(str, sorted(myString.split('x'))))
+
+    return sorted(s for s in myString.split('x') if s)
