3c36b5a2c11bd4d398cd37492f65282d8ab60332
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-24 01:09:01 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,8 +1,8 @@
 function solution(n) {
-    const sqrt = Math.floor(Math.sqrt(n))
+    const sqrt = Math.sqrt(n);
     let result = 0;
-    for(let i = 1; i <= n; i++){
+    for(let i = 1; i <= sqrt; i++){
         if(n % i === 0) result++
     }
-    return  result
+    return sqrt === Math.floor(sqrt) ? result * 2 - 1 : result * 2
 }
