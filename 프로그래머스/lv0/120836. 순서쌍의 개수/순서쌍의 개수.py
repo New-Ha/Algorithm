34d9d38826a9240ea06d2cef896d604fd0f37898
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-24 01:12:27 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -2,3 +2,5 @@ def solution(n):
     sqrt = int(n ** 0.5)
     result = len([i for i in range(1, sqrt + 1) if n % i == 0]) * 2
     return  result - 1 if sqrt == n ** 0.5 else result
+
+    #return len(list(filter(lambda v: n % (v+1) == 0, range(n))))
