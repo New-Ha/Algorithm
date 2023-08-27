641cf50ec602870a54afef982d9d27ccde0b34a0
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-27 16:57:21 +0900

    [unrated] Title: 글자 지우기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,3 +1,4 @@
 function solution(my_string, indices) {
-    return my_string.split('').map((el, idx) => indices.includes(idx) ? el = '' : el).join('')
+    // return my_string.split('').map((el, idx) => indices.includes(idx) ? el = '' : el).join('')
+    return my_string.split('').filter((_, idx) => !indices.includes(idx)).join('')
 }
