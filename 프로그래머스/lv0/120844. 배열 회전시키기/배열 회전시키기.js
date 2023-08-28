0932257349935d40b05ed019b0ab926f32cb9904
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-28 15:07:57 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,11 +1,4 @@
 function solution(numbers, direction) {
-    if (direction === 'right'){
-        const last = numbers.pop()
-        numbers.unshift(last)
-        return numbers
-    }else{
-        const first = numbers.shift()
-        numbers.push(first)
-        return numbers
-    }
+    direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift()) 
+    return numbers
 }
