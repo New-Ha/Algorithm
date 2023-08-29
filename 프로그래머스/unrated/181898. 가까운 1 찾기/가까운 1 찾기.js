a5d2f2e163789d4d8f9e53d48aa17f5c06232b95
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-29 17:50:48 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -2,5 +2,5 @@ function solution(arr, idx) {
     arr = arr.slice(idx);
     return arr.indexOf(1) === -1 ? -1 : arr.indexOf(1) + idx
 
-    // return a.indexOf(1,i)
+    // return arr.indexOf(1,i)
 }
