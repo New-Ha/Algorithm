2cdba93ca5762b708633c3110afffcf249c4d5d1
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-30 16:12:15 +0900

    [unrated] Title: 세 개의 구분자, Time: 115.49 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -2,4 +2,8 @@ function solution(myStr) {
     const remove = ['a', 'b', 'c'];
     const separate =  myStr.split('').map(el => remove.includes(el) ? el = '/' : el).join('').split('/').filter(el => el !== '')
     return separate.length === 0 ? ["EMPTY"] : separate
+    
+    //ver 2
+    // const arr = myStr.split(/[abc]/).filter(el => el);
+    // return arr.length == 0 ? ["EMPTY"] : arr;
 }
