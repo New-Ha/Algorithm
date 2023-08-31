5210ae94326af82cf0ae09afa901924cdb6fe395
UHa <141496181+New-Ha@users.noreply.github.com>
2023-08-31 16:11:52 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,7 +1,14 @@
 def solution(str_list):
-    haveStr = [21, 21]
-    if 'l' in str_list:
-        haveStr[0] = str_list.index('l')
-    if 'r' in str_list:
-        haveStr[1] = str_list.index('r')
-    return [] if haveStr == [-1, -1] else str_list[0: haveStr[0]] if haveStr[0] < haveStr[1] else str_list[haveStr[1] + 1:]
+    # ver 1
+    # haveStr = [21, 21]
+    # if 'l' in str_list:
+    #     haveStr[0] = str_list.index('l')
+    # if 'r' in str_list:
+    #     haveStr[1] = str_list.index('r')
+    # return [] if haveStr == [-1, -1] else str_list[: haveStr[0]] if haveStr[0] < haveStr[1] else str_list[haveStr[1] + 1:]
+    
+    # ver 2
+    for i in range(len(str_list)):
+        if str_list[i] == 'l': return str_list[:i]
+        if str_list[i] == 'r': return str_list[i + 1:]
+    return []
