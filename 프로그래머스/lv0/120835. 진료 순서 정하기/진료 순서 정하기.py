de9495819bda2c77fcf1933ea091a55ae29ee66c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-01 17:46:21 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,3 +1,4 @@
 def solution(emergency):
     rank = sorted(emergency, reverse = True)
-    return list(map(lambda el: rank.index(el) + 1, emergency))
+    # return list(map(lambda el: rank.index(el) + 1, emergency))
+    return [rank.index(i) + 1 for i in emergency]
