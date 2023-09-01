2d721295ba4f0fd8ebac6f7b79bfa2d05a73397a
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-01 17:07:41 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,5 +1,4 @@
 function solution(quiz) {
     return quiz.map(el => el.split(' '))
-        .map(([x, d, y, _, z]) => d === '+' && Number(x) + Number(y) === Number(z)           ? "O" 
-          : d === '-' && Number(x) - Number(y) === Number(z) ? "O" : "X")
+        .map(([x, d, y, _, z]) => d === '+' && Number(x) + Number(y) === Number(z)           ? "O" : d === '-' && Number(x) - Number(y) === Number(z) ? "O" : "X")
 }
