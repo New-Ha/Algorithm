3da4e7bdd929f1c697286c88f0955807b3350ca2
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-01 16:41:16 +0900

    [level 0] Title: k의 개수, Time: 14.59 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,2 +1,3 @@
 def solution(i, j, k):
-    return sum([1 for n in range(i, j + 1) for y in list(str(n)) if str(k) in y])
+    # return sum([1 for n in range(i, j + 1) for x in list(str(n)) if str(k) in x])
+    return sum([ str(n).count(str(k)) for n in range(i, j + 1)])
