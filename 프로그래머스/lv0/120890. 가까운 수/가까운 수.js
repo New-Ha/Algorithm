b4d0f29c28cba7c6cc292a5bc5a1b700f47df62c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-01 17:18:48 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,5 +1,8 @@
 function solution(array, n) {
-    let sorted = array.sort((a,b) => a-b);
-    let abs = sorted.map((el) => Math.abs(n-el));
-    return array[abs.indexOf(Math.min(...abs))] 
+//     let sorted = array.sort((a,b) => a-b);
+//     let abs = sorted.map((el) => Math.abs(n-el));
+//     return array[abs.indexOf(Math.min(...abs))] 
+    
+    array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+    return array[0]
 }
