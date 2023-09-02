7f26283739e2752899d59914e02c5cc0655b72da
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-02 13:28:30 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.33 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,4 +1,15 @@
 function solution(n) {
-    return result = Array(n).fill(0).map((_, i) => Array(n).fill(0).map((el, idx) => i === idx ? el = 1 : el))
+    //ver 1
+    return result = Array(n).fill(0)
+        .map((_, i) => Array(n).fill(0).map((el, idx) => i === idx ? el = 1 : el))
     
+    //ver 2
+    // const result = [];
+    // for(let i = 0; i < n; i++){
+    //     result.push([]);
+    //     for(let j = 0; j < n; j++){
+    //         i === j ? result[i].push(1) : result[i].push(0);
+    //     }
+    // }
+    // return result
 }
