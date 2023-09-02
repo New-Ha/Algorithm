c0eb30cc384d1fabe5992521a84e54f1ff28875c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-02 14:48:07 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,6 @@
 function solution(num, k) {
-    const result = String(num).indexOf(k);
-    return result > -1 && result + 1 || result
+    // const result = String(num).indexOf(k);
+    // return result > -1 && result + 1 || result
+    
+    return String(num).indexOf(k) + 1 || -1
 }
