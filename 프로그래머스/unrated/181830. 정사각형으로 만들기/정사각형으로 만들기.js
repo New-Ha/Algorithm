a6157d02f6e1cd3aad6825b629ae8faa57436692
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-04 01:53:46 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.06 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    const row = arr.length;
+    const column = arr[0].length;
+    
+    if(row > column){
+        return arr.map(el => [...el, ...Array(row - column).fill(0)])
+    }else if(row < column){
+        return [...arr, ...Array(column - row).fill(Array(column).fill(0))]
+    }else return arr
+}
