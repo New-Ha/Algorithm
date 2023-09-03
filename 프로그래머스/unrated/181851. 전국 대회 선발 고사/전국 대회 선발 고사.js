def27174e3b386a6a8ed3cf50cb94c3f059172d4
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-04 01:43:52 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,8 +1,11 @@
 function solution(rank, attendance) {
-    const possible = {}
-    attendance.map((el, idx) => el ? possible[idx] = rank[idx] : "")
-    const pickArr = Object.entries(possible).sort((a, b) => a[1] - b[1])
-    return 10000 * Number(pickArr[0][0]) + 100 * Number(pickArr[1][0]) + Number(pickArr[2][0])
+    //ver 1
+    // const possible = {}
+    // attendance.map((el, idx) => el ? possible[idx] = rank[idx] : "")
+    // const pickArr = Object.entries(possible).sort((a, b) => a[1] - b[1])
+    // return 10000 * Number(pickArr[0][0]) + 100 * Number(pickArr[1][0]) + Number(pickArr[2][0])
     
-                                    
+    //ver 2
+    const possible = rank.map((el, idx) => [el, idx, attendance[idx]]).filter(el => el[2] === true).sort((a, b) => a[0] - b[0]);
+    return 10000 * possible[0][1] + 100 * possible[1][1] + possible[2][1]
 }
