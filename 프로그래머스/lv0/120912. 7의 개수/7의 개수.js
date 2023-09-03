011a26c8d0c52b1458e8674783dbb946d657505c
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-04 02:51:21 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,3 +1,5 @@
 function solution(array) {
-    return array.join('').split('').reduce((acc, cur) => cur === '7' ? acc + 1 : acc , 0)
+//     return array.join('').split('').reduce((acc, cur) => cur === '7' ? acc + 1 : acc , 0)
+    
+    return array.join('').split('').filter(el => el === '7').length
 }
