3416190c35148419b60a529b45fbc3d6ca2c7b17
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-07 14:09:16 +0900

    [level 2] Title: 주식가격, Time: 134.32 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
 
 ### 성능 요약
 
-메모리: 42.4 MB, 시간: 8.79 ms
+메모리: 17 MB, 시간: 134.32 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,11 @@
+def solution(prices):
+    result = []
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[i] > prices[j]:
+                result.append(j - i)
+                break
+            elif prices[i] <= prices[j] and j == len(prices) - 1:
+                result.append(j - i)
+    result.append(0)
+    return result
