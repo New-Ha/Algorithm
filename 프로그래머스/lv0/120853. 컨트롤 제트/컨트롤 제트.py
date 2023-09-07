b4fc49a407e7686050517d9bb081903e8fd069ab
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-07 14:48:10 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -2,7 +2,6 @@ def solution(s):
     strList = s.split(' ');
     for i in range(len(strList)):
         if strList[i] == 'Z':
-            strList[i - 1] = 0
-            strList[i] = 0
+            strList[i] = int(strList[i - 1]) * -1
         else: strList[i] = int(strList[i])
     return sum(strList)
