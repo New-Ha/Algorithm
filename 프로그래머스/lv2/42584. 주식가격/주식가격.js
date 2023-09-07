fe294e1621fb7a3075a18ff1c42a9990c3999e34
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-07 14:08:04 +0900

    [level 2] Title: 주식가격, Time: 8.79 ms, Memory: 42.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -0,0 +1,18 @@
+function solution(prices) {
+    //ver 1
+    const result = [];
+    for(let i = 0; i < prices.length; i++){
+        for(let j = i + 1; j < prices.length; j++){
+            if(prices[i] > prices[j]){
+                result.push(j - i);
+                break;
+            }else if(prices[i] <= prices[j] && j === prices.length -1){
+                result.push(j - i);
+            }
+        }
+    }
+    result.push(0)
+    return result
+    
+    
+}
