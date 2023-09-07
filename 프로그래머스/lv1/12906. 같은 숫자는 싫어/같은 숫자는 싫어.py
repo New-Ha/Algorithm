01c812f5a3538ef701f5f544a2d7dded41f814cf
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-07 15:39:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 121.56 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,10 +1,17 @@
 def solution(arr):
-    stack = [];
-    while len(arr):
-        if len(stack) == 0:
-            stack.append(arr.pop());
-        else:
-            if stack[-1] != arr[-1]:
-                stack.append(arr.pop())
-            else: stack.pop()
-    return list(reversed(stack))
+    # stack = [];
+    # while len(arr):
+    #     if len(stack) == 0:
+    #         stack.append(arr.pop());
+    #     else:
+    #         if stack[-1] != arr[-1]:
+    #             stack.append(arr.pop())
+    #         else: stack.pop()
+    # return list(reversed(stack))
+
+    result = []
+    for el in arr:
+        if len(result) == 0 or result[-1] != el:
+            result.append(el)
+
+    return result
