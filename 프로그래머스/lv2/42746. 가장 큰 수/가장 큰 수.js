73937a5c2557b5789c40ce923e96b7d4f31809ec
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-12 01:16:51 +0900

    [level 2] Title: 가장 큰 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,6 @@
 function solution(numbers) {
     const sorted = numbers.map(el => String(el).repeat(3)).sort().reverse();
-    const result = sorted.map(el => el.slice(0, el.length / 3)).join('')
+    const result = sorted.map(el => el.slice(0, el.length / 3)).join('');
     return Number(result) === 0 ? "0" : result
    
 }
