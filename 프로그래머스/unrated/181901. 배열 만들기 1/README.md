1a0b807c12ce3640cbc2f1d1ee6b47357a270595
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-15 09:40:45 +0900

    [unrated] Title: 배열 만들기 1, Time: 8.52 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/README.md
+++ 프로그래머스/unrated/181901. 배열 만들기 1/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열 만들기 1 - 181901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181901) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 5.16 ms
+메모리: 9.99 MB, 시간: 8.52 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,5 +1,7 @@
 def solution(n, k):
-    result = [];
-    for i in range(k, n + 1):
-        if i % k == 0: result.append(i);
-    return result
+    # result = [];
+    # for i in range(k, n + 1):
+    #     if i % k == 0: result.append(i);
+    # return result
+    
+    return [x for x in range(k, n + 1) if x % k == 0]
