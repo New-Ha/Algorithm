b6123785835f114bfe66f2973f693935fd0507b9
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-18 15:07:17 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,7 +1,6 @@
 def solution(a, b, n):
     cokes = 0;
     while n >= a:
-        temp = (n // a);
-        cokes += temp * b;
-        n = n - (temp * a) + (temp * b);
+        cokes += (n // a) * b;
+        n = (n % a) + ((n // a) * b);
     return cokes
