82dc1e8dc9d95991b6abdcad7b208f526bcd5c83
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-20 03:39:42 +0900

    [level 2] Title: 타겟 넘버, Time: 1.81 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,40 @@
-function solution(numbers, target) {
+// ver 1
+function bfs(numbers) {
     let cases = [0];
-    for(let i of numbers){
-        const temp = [];
-        for(let j of cases){
-            temp.push(j + i);
-            temp.push(j - i);
-        }
-        cases = temp;
+    for (let i of numbers) {
+      const temp = [];
+      for (let j of cases) {
+        temp.push(j + i);
+        temp.push(j - i);
+      }
+      cases = temp;
     }
-    let count = 0;
-    for(let i of cases){
-        if(i === target) count++;
-    }
-    return count
+    return cases;
+  }
+
+function solution(numbers, target) { 
+  let count = 0;
+  const cases = bfs(numbers);
+  for (let i of cases) {
+    if (i === target) count++;
+  }
+  return count;
 }
+
+//ver 2
+// function solution(numbers, target) {
+//     let cases = [0];
+//     for(let i of numbers){
+//         const temp = [];
+//         for(let j of cases){
+//             temp.push(j + i);
+//             temp.push(j - i);
+//         }
+//         cases = temp;
+//     }
+//     let count = 0;
+//     for(let i of cases){
+//         if(i === target) count++;
+//     }
+//     return count
+// }
