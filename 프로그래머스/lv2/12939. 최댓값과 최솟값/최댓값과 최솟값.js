6dab41dd1146ececa437518442ad23c5a3f765cc
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-22 13:49:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,7 @@
 function solution(s) {
-    const numArr = s.split(' ').map(el => Number(el)).sort((a, b) => a - b);
-    return `${numArr[0]} ${numArr[numArr.length - 1]}`
+    // const numArr = s.split(' ').map(el => Number(el)).sort((a, b) => a - b);
+    // return `${numArr[0]} ${numArr[numArr.length - 1]}`
+    
+    const splited = s.split(' ');
+    return Math.min(...splited)+ ' ' + Math.max(...splited)
 }
