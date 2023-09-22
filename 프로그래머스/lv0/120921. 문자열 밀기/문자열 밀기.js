e11255d60e4d9ea674a1a81c0b581f2126c47b2b
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-22 14:14:47 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,8 @@
 function solution(A, B) {
-    if(A === B) return 0;
-    A = A.split('')
-    for(let i = 0; i < A.length; i++){
-        let last = A.pop();
-        A.unshift(last);
-        if(A.join('') === B) return i + 1;
+    if (A === B) return 0;
+    for (let i = 0; i < A.length; i++) {
+        A = A.slice(-1) + A.slice(0, -1);
+        if (A === B) return i + 1;
     }
     return -1;
 }
