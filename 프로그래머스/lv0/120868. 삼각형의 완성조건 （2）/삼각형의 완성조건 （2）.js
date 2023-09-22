c3314ddf68e9d0331e80de6311a01c5c3b3a6b39
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-22 14:02:44 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,4 +1,4 @@
 function solution(sides) {
-    const sorted = sides.sort((a, b) => a- b);
-    return 2 * sorted[0] - 1;
+    // const sorted = sides.sort((a, b) => a- b);
+    return 2 * Math.min(...sides) - 1;
 }
