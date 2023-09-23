8b7986ffe796ffb2eb68c142ef457327afdbe706
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-23 22:22:59 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,8 @@
+function solution(id_pw, db) {
+    for(let [id, pw] of db){
+        if(id_pw[0] === id){
+            return id_pw[1] === pw ? "login" : "wrong pw"
+        }
+    }
+    return "fail"
+}
