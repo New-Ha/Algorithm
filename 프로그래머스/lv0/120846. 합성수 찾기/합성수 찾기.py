92734b8af671eac9deaa135be50ff5aed40856ac
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-27 16:27:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,11 +1,19 @@
 def solution(n):
-    if n < 4: return 0
-    result = n // 2 - 1;
-    for i in range(5, n + 1, 2):
-        temp = 0;
-        for j in range(1, i + 1):
-            if i % j == 0: temp += 1;
-            if temp == 3:
-                result += 1;
-                break;
-    return result
+    # if n < 4: return 0
+    # result = n // 2 - 1;
+    # for i in range(5, n + 1, 2):
+    #     temp = 0;
+    #     for j in range(1, i + 1):
+    #         if i % j == 0: temp += 1;
+    #         if temp == 3:
+    #             result += 1;
+    #             break;
+    # return result
+    
+    output = 0
+    for i in range(4, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                output += 1
+                break
+    return output
