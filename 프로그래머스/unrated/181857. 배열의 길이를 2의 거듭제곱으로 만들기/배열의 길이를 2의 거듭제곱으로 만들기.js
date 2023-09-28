41f53ed7240e9d13612c661564499b0f25f2366a
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-28 16:13:26 +0900

    [unrated] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
+++ 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
@@ -1,6 +1,8 @@
 function solution(arr) {
-    const powNum = new Array(10).fill().map((_, idx)=> (2 ** (idx+1))).filter(el => el >= arr.length)[0];
-    const result = Array(powNum).fill(0).map((_, idx) => arr[idx] ? arr[idx] : 0);
+    // const powNum = new Array(10).fill().map((_, idx)=> (2 ** (idx+1))).filter(el => el >= arr.length)[0];
+    
+    const lenNum = 2 ** Math.ceil(Math.log2(arr.length));
+    const result = Array(lenNum).fill(0).map((_, idx) => arr[idx] ? arr[idx] : 0);
     
     return arr.length === 1 ? arr : result
 }   
