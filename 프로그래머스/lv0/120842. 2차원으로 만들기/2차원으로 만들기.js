00a2c715ac51686697cefd353e09f265dbd80587
UHa <141496181+New-Ha@users.noreply.github.com>
2023-09-29 13:58:10 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,9 +1,18 @@
 function solution(num_list, n) {
-    return num_list.map((el, idx) => {
-        if(idx % n === 0){
-            return num_list.slice(idx, idx+n)
-        }
-    }).filter(el => el !== undefined)
+    // ver 1
+    // return num_list.map((el, idx) => {
+    //     if(idx % n === 0){
+    //         return num_list.slice(idx, idx+n)
+    //     }
+    // }).filter(el => el !== undefined)
+    
+    //ver 2
+    const result = [];
+    for(let i = 0; i < num_list.length / n; i++){
+        result.push(num_list.slice(n*i, n*i+n))
+    }
+    return result
     
     // return Array(num_list.length / n).fill([]).map(() => num_list.splice(0, n))
+    
 }
