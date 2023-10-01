7eb13c942f6fe4c075471dc83b8b30a9271396c1
UHa <141496181+New-Ha@users.noreply.github.com>
2023-10-01 16:33:23 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,8 +1,12 @@
 function solution(before, after) {
-    const str1 = before.split('').sort()
-    const str2 = after.split('').sort()
-    for(let i = 0; i < str1.length; i++){
-        if(str1[i] !== str2[i]) return 0
-    }
-    return 1
+    // ver 1
+    // const str1 = before.split('').sort()
+    // const str2 = after.split('').sort()
+    // for(let i = 0; i < str1.length; i++){
+    //     if(str1[i] !== str2[i]) return 0
+    // }
+    // return 1
+    
+    //ver 2
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0
 }
