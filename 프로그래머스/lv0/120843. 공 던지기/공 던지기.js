b273b3c090903269ca9ccd70e2e7194c32bf6186
UHa <141496181+New-Ha@users.noreply.github.com>
2023-10-04 16:40:52 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,10 +1,19 @@
 function solution(numbers, k) {
-    let count = 1;
-    while(count < k){
-        for(let i = 0; ;i +=2){
-            i %= numbers.length;
-            if(count === k) return numbers[i];
-            count++;
+//     let count = 1;
+//     while(count < k){
+//         for(let i = 0; ;i +=2){
+//             i %= numbers.length;
+//             if(count === k) return numbers[i];
+//             count++;
+//         }
+//     }
+    
+    let idx = 0
+    for(let count = 1; count < k; count++){
+        idx += 2;
+        if(idx > numbers.length){
+            idx %= numbers.length
         }
     }
+    return numbers[idx];
 }
