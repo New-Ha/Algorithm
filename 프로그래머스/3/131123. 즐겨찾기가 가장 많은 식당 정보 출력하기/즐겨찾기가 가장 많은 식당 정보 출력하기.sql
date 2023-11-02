6a26a2b48e035a2e2a2ef4fab4b9528c2a2ffd1e
UHa <141496181+New-Ha@users.noreply.github.com>
2023-11-02 20:45:21 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,5 @@
+SELECT food_type, rest_id, rest_name, favorites FROM rest_info
+WHERE favorites IN (SELECT MAX(favorites) FROM rest_info
+                  GROUP BY food_type)
+GROUP BY food_type
+ORDER BY food_type DESC
