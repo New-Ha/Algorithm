bc06d55cfdf3f3d1d6db9253947027deb7e0e4f2
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-01 14:32:02 +0900

    [level 1] Title: 최소직사각형, Time: 3.54 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,12 +1,13 @@
 def solution(sizes):
-    row = [];
-    column = [];
+    row = [max(i) for i in sizes];
+    column = [min(i) for i in sizes];
     
-    for r, c in sizes:
-        if r > c:
-            row.append(r);
-            column.append(c);
-        else:
-            row.append(c);
-            column.append(r);
+    # for r, c in sizes:
+    #     if r > c:
+    #         row.append(r);
+    #         column.append(c);
+    #     else:
+    #         row.append(c);
+    #         column.append(r);
+    
     return max(row) * max(column)
