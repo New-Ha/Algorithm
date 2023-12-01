6b70d2e588da5ba45c0be31d7585940326145b1a
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-01 14:33:39 +0900

    [level 1] Title: 최소직사각형, Time: 2.21 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 3.54 ms
+메모리: 38 MB, 시간: 2.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 14:31:59
+2023년 12월 5일 14:33:36
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,15 +1,20 @@
 function solution(sizes) {
     const row = [];
     const column = [];
-    sizes.forEach(([r, c]) => {
-        if(r > c){
-            row.push(r)
-            column.push(c)
-        }
-        else{
-            row.push(c)
-            column.push(r)
-        }
+    // sizes.forEach(([r, c]) => {
+    //     if(r > c){
+    //         row.push(r)
+    //         column.push(c)
+    //     }
+    //     else{
+    //         row.push(c)
+    //         column.push(r)
+    //     }
+    // })
+    
+    sizes.forEach(el => {
+        row.push(Math.max(...el));
+        column.push(Math.min(...el));
     })
     
     return Math.max(...row) * Math.max(...column)
