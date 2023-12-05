77b55c4c2c1e800b479fa38217197db12f9b0b72
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-05 15:34:02 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -3,6 +3,7 @@ def solution(brown, yellow):
     factors = [];
     for i in range(2, int(area ** 0.5) + 1):
         if area % i == 0:
-            factors.append(i)
-    result = [area / i for i in factors if (i - 2) * (area / i - 2) == yellow]
-    return [result[0], area / result[0]]
+            if (i - 2) * (area / i - 2) == yellow:
+                factors.append(area // i)
+                factors.append(i)
+    return factors
