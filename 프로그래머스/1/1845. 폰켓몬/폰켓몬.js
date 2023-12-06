f43a68c7040ad2d75776c4060409aca7b1bb5649
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-07 01:09:12 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -2,6 +2,5 @@ function solution(nums) {
     const monNum = Math.floor(nums.length / 2)
     const monKind = new Set(nums)
     
-    if(monNum <= monKind.size) return monNum
-    if(monNum > monKind.size) return monKind.size
+    return monNum <= monKind.size ? monNum : monKind.size
 }
