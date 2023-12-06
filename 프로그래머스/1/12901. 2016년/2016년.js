14bca880a55f3c16d193d08fb54d0274fdb5c78e
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-06 15:53:51 +0900

    [level 1] Title: 2016년, Time: 2.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    const day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    
+    let daytime = b - 1;
+    for(let i = 0; i < a - 1; i++){
+        daytime += month[i]
+    }
+    console.log(daytime)
+    return day[daytime % 7]
+}
