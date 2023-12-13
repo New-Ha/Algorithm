a074b925ab9e16269ec5411d54b2fda7cc805bac
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-13 15:57:09 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 1:35:28
+2023년 12월 3일 15:57:4
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,13 +1,19 @@
 function solution(lottos, win_nums) {
-    let countZero = 0;
-    let countMatch = 0;
-    lottos.forEach(el => {
-        if(el === 0) countZero++;
-        if(win_nums.includes(el)) countMatch++;
-    })
+    //ver 1
+//     let countZero = 0;
+//     let countMatch = 0;
+//     lottos.forEach(el => {
+//         if(el === 0) countZero++;
+//         if(win_nums.includes(el)) countMatch++;
+//     })
     
-    if(countZero === 6) return [1, 6]
-    if(countMatch === 0) return [6, 6]
-    else return [7 - countMatch - countZero, 7 - countMatch]
+//     if(countZero === 6) return [1, 6]
+//     if(countMatch === 0) return [6, 6]
+//     else return [7 - countMatch - countZero, 7 - countMatch]
 
+    //ver2
+    const rank = [6, 6, 5, 4, 3, 2, 1, 1]
+    const zeroCount = lottos.filter(el => el === 0).length
+    const matchCount = lottos.filter(el => win_nums.includes(el)).length
+    return [rank[zeroCount + matchCount], rank[matchCount]]
 }
