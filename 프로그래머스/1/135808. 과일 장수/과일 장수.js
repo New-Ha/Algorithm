04e71ff3c6e59e1a4cf3ce7a2eb4fc38a1e1ab44
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-13 16:30:20 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,11 +1,18 @@
 function solution(k, m, score) {
     score.sort((a, b) => b - a);
+    //ver 1
+    // const package = [];
+    // let start = 0
+    // for(let i = m; i <= score.length; i += m){
+    //     package.push(score.slice(start, i));
+    //     start = i;
+    // }
+    // return package.reduce((acc, cur) => acc + (Math.min(...cur) * m), 0)
     
-    const package = [];
-    let start = 0
-    for(let i = m; i <= score.length; i += m){
-        package.push(score.slice(start, i));
-        start = i;
+    //ver  2
+    let revenue = 0;
+    for(let i = m - 1; i < score.length; i += m){
+        revenue += score[i] * m
     }
-    return package.reduce((acc, cur) => acc + (Math.min(...cur) * m), 0)
+    return revenue
 }
