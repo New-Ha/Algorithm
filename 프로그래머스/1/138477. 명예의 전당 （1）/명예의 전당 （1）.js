a4e20178a2c607a909b2202e54b08802abb26d62
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-17 17:45:06 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,8 +1,9 @@
 function solution(k, score) {
     const result = [];
     for(let i = 1; i <= score.length; i++){
-        if(i <= k) result.push(Math.min(...score.slice(0, i)))
-        else result.push(Math.min(...score.slice(0, i).sort((a, b) => b - a).splice(0, k)))
+        let temp = score.slice(0, i).sort((a, b) => b - a)
+        if(i <= k) result.push(temp[i - 1]);
+        else result.push(temp[k - 1])
     }
     return result
 }
