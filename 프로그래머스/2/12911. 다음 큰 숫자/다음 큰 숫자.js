f47cee7f0d3c577c872f3a16f31e20ca0232ada6
UHa <141496181+New-Ha@users.noreply.github.com>
2023-12-19 18:36:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,5 +1,6 @@
 function solution(n) {
-    const oneCount = n.toString(2).split('').filter(el => el === '1').length;
+    // const oneCount = n.toString(2).split('').filter(el => el === '1').length;
+    const oneCount = n.toString(2).split('').reduce((acc, cur) => cur === '1' ? acc + 1 : acc, 0);
     let result = n + 1;
     
     while(result > n){
