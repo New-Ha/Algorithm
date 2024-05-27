9f3618754a3dadc0d9549398e3d94f157dc71c0c
UHa <141496181+New-Ha@users.noreply.github.com>
2024-05-27 21:19:54 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,13 +1,18 @@
 function solution(strings, n) {
-    const sortedStr = strings.sort();
-    const nSorted = sortedStr.map((el, idx) => [el[n], idx]).sort((a, b) => {
-        if(a[0] === b[0]) {
-            return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0
-        }else if(a[0] > b[0]) {
-            return 1;
-        }else {
-            return -1;
-        }
-    })
-    return nSorted.map(([_, idx]) => sortedStr[idx])
+// case 1)
+//     const sortedStr = strings.sort();
+//     const nSorted = sortedStr.map((el, idx) => [el[n], idx]).sort((a, b) => {
+//         if(a[0] === b[0]) {
+//             return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0
+//         }else if(a[0] > b[0]) {
+//             return 1;
+//         }else {
+//             return -1;
+//         }
+//     })
+    
+//     return nSorted.map(([_, idx]) => sortedStr[idx])
+
+// case 2)
+    return strings.map((el) => el[n] + el).sort().map(el => el.slice(1));
 }
