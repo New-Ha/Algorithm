6ba9aac20bf9a70d7043349e6bb97675e257dac7
UHa <141496181+New-Ha@users.noreply.github.com>
2024-06-05 15:03:17 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,11 +1,26 @@
-SELECT EMP_NO, EMP_NAME, CASE WHEN AVG(SCORE) >= 96 THEN 'S'
-                              WHEN AVG(SCORE) >= 90 THEN 'A'
-                              WHEN AVG(SCORE) >= 80 THEN 'B'
-                              ELSE 'C' END AS GRADE,
-                        CASE WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
-                             WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
-                             WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
-                             ELSE SAL * 0 END AS BONUS
-FROM HR_EMPLOYEES HE INNER JOIN HR_GRADE HG USING (EMP_NO)
-GROUP BY EMP_NO
+-- try 1
+# SELECT EMP_NO, EMP_NAME, CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+#                               WHEN AVG(SCORE) >= 90 THEN 'A'
+#                               WHEN AVG(SCORE) >= 80 THEN 'B'
+#                               ELSE 'C' END AS GRADE,
+#                         CASE WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+#                              WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+#                              WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
+#                              ELSE SAL * 0 END AS BONUS
+# FROM HR_EMPLOYEES HE INNER JOIN HR_GRADE HG USING (EMP_NO)
+# GROUP BY EMP_NO
+# ORDER BY EMP_NO;
+
+-- try 2
+SELECT EMP_NO, EMP_NAME, GRADE, CASE GRADE WHEN 'S' THEN SAL * 0.2
+                                           WHEN 'A' THEN SAL * 0.15
+                                           WHEN 'B' THEN SAL * 0.1
+                                           ELSE SAL * 0 END AS BONUS
+FROM HR_EMPLOYEES HE 
+    INNER JOIN (SELECT EMP_NO, CASE WHEN AVG(SCORE) >= 96 THEN 'S'
+                    WHEN AVG(SCORE) >= 90 THEN 'A'
+                    WHEN AVG(SCORE) >= 80 THEN 'B'
+                    ELSE 'C' END AS GRADE
+    FROM HR_GRADE
+    GROUP BY EMP_NO) HB USING (EMP_NO)
 ORDER BY EMP_NO;
