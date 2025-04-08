2221ff504915ebdd9516ece1d1fdb77f4f12f28d
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-08 17:56:51 +0900

    [Silver II] Title: 쇠막대기, Time: 188 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.js
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 const stack = [];
 let count = 0;
 
-for(let i = 0; i < input.length; i++){
-    if(input[i] === '('){
-        stack.push(input[i])
-    } 
-    
-    if(input[i] === ')' && input[i - 1] === '('){
+for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(') {
+        stack.push('(');
+    } else if (input[i - 1] === '(') {
         stack.pop();
         count += stack.length;
-    } 
-    
-    if (input[i] === ')' && input[i - 1] === ')') {
+    } else {
         stack.pop();
-        count += 1
+        count += 1;
     }
 }
 
-console.log(count)
+console.log(count);
