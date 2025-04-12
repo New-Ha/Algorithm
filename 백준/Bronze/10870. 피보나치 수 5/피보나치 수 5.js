f00214e9f601a849f7f6d2497bcbc723167c65ff
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-12 21:11:45 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 148 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+let n = Number(input);
+
+const fibo = (n) => {
+    if(n === 0) return 0;
+    if(n === 1) return 1;
+    return fibo(n - 1) + fibo(n - 2)
+}
+
+console.log(fibo(n))
