45e5e2acd430203e869315329af294640c83f884
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-13 14:24:21 +0900

    [Silver IV] Title: 보물, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.js
+++ 백준/Silver/1026. 보물/보물.js
@@ -6,14 +6,9 @@ const arrA = input[1].split(' ').map(Number).sort((a, b) => a - b);
 const arrB = input[2].split(' ').map(Number);
 const indexB = arrB.map((val, idx) => ({val, idx})).sort((a, b) => b.val - a.val);
 
-const newA = [];
-for (let i = 0; i < N; i++) {
-  newA[indexB[i].idx] = arrA[i];
-}
-
 let sum = 0;
-for(let i = 0; i < N; i++){
-    sum += newA[i] * arrB[i]
+for (let i = 0; i < N; i++) {
+  sum += arrA[i] * indexB[i].val;
 }
 
 console.log(sum)
