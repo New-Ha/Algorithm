a7d2c994b70abfe87394c5275ea68687fe3f2c65
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-13 19:57:46 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,18 +1,45 @@
 function solution(n, lost, reserve) {
-    const both = lost.filter(student => reserve.includes(student));
-    const realLost = lost.filter(student => !both.includes(student)).sort((a, b) => a - b);
-    const realReserve = new Set(reserve.filter(student => !both.includes(student)));
+// case 1
+//     const both = lost.filter(student => reserve.includes(student));
+//     const realLost = lost.filter(student => !both.includes(student)).sort((a, b) => a - b);
+//     const realReserve = new Set(reserve.filter(student => !both.includes(student)));
     
-    let count = realLost.length;
-    for(let student of realLost) {
-	    if(realReserve.has(student - 1)) {
-		    realReserve.delete(student - 1);
-		    count--;
-	    } else if (realReserve.has(student + 1)){
-		    realReserve.delete(student + 1);
-		    count--;
-	    }
+//     let count = realLost.length;
+//     for(let student of realLost) {
+// 	    if(realReserve.has(student - 1)) {
+// 		    realReserve.delete(student - 1);
+// 		    count--;
+// 	    } else if (realReserve.has(student + 1)){
+// 		    realReserve.delete(student + 1);
+// 		    count--;
+// 	    }
+//     }
+    
+//     return n - count;
+    
+// case 2
+    const student = new Array(n + 2).fill(0); 
+
+    for (let r of reserve) {
+        student[r]++;
     }
-    
-    return n - count;
+
+    for (let l of lost) {
+        student[l]--;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        if (student[i] === -1) {
+            if (student[i - 1] === 1) {
+                student[i]++;
+                student[i - 1]--;
+            } else if (student[i + 1] === 1) {
+                student[i]++;
+                student[i + 1]--;
+            }
+        }
+    }
+
+    return student.slice(1, n + 1).filter(x => x >= 0).length;
+
 }
