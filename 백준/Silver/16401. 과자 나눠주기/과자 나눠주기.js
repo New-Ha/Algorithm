fe6dd4cf06a3cf19559b5f3f4675fb3355e124cc
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-14 21:07:13 +0900

    [Silver II] Title: 과자 나눠주기, Time: 656 ms, Memory: 133460 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.js
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.js
@@ -4,20 +4,16 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [M, N] = input[0].split(' ').map(Number);
 const snacks = input[1].split(' ').map(Number);
 
-let start = 1
+let start = 1;
 let end = Math.max(...snacks);
-let mid = 0;
 let answer = 0;
 
 while(start <= end){
-    mid = Math.floor((start + end) / 2)
+    const mid = Math.floor((start + end) / 2);
     let count = 0;
+    
     for(let snack of snacks){
-        if(snack < mid) {
-            continue;
-        } else {
-            count += Math.floor(snack / mid)
-        }
+        count += Math.floor(snack / mid)
     }
     
     if(count >= M) {
