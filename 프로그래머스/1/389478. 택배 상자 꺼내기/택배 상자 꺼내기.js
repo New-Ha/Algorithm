2a7eac25008d6c637f2dbfa68eea6e4a8bd5add1
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-16 14:42:46 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,35 +1,55 @@
 function solution(n, w, num) {
     const h = Math.ceil(n / w)
-    const boxs = Array.from({ length: h }, () => Array(w).fill(0));
-    let findX;
-    let findY;
+
+// case 1. simulation
+//     const boxs = Array.from({ length: h }, () => Array(w).fill(0));
+//     let findX;
+//     let findY;
     
-    for(let i = 0; i < h; i++){
-        for(let j = 0; j < w; j++){
-            if(i % 2 === 0){
-                let temp = i * w + j + 1
-                if(temp <= n ){
-                    boxs[i][j] = i * w + j + 1
-                }
-                if(temp === num){
-                    findX = j;
-                    findY = i;
-                }
-            } else {
-                let temp = i * w + (w - j)
-                if(temp <= n){
-                    boxs[i][j] = i * w + (w - j)
-                }
-                if(temp === num){
-                    findX = j;
-                    findY = i;
-                }
-            }
-        }
+//     for(let i = 0; i < h; i++){
+//         for(let j = 0; j < w; j++){
+//             if(i % 2 === 0){
+//                 let curN = i * w + j + 1
+//                 if(curN <= n ){
+//                     boxs[i][j] = i * w + j + 1
+//                 }
+//                 if(curN === num){
+//                     findX = j;
+//                     findY = i;
+//                 }
+//             } else {
+//                 let curN = i * w + (w - j)
+//                 if(curN <= n){
+//                     boxs[i][j] = i * w + (w - j)
+//                 }
+//                 if(curN === num){
+//                     findX = j;
+//                     findY = i;
+//                 }
+//             }
+//         }
+//     }
+//     let count = 0;
+//     for(let i = findY; i < h; i++){
+//         if(boxs[i][findX] !== 0) count++
+//     }
+//     return count
+    
+// case 2
+    let row = Math.floor((num - 1) / w);
+    let col;
+    if(row % 2 === 0){
+        col = (num - 1) % w
+    } else {
+        col = w - 1 - (num - 1) % w
     }
     let count = 0;
-    for(let i = findY; i < h; i++){
-        if(boxs[i][findX] !== 0) count++
+    for(let i = row; i < h; i++){
+        if(i % 2 === 0 && (i * w + col) <= n){
+            count++
+        } else if(i % 2 !== 0 && (i * w + (w - col)) <= n){
+            count++
+        }
     }
     return count
 }
