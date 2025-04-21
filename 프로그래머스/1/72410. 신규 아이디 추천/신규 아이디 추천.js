86ff19766e53afee1321add07f03ff4e9193d7b1
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-21 22:15:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,26 +1,29 @@
 function solution(new_id) {
-    let id = new_id.toLowerCase();
+  let id = '';
+  const allowed = 'abcdefghijklmnopqrstuvwxyz0123456789-_.';
+  let prevChar = '';  
+  for (let c of new_id.toLowerCase()) {
+    if (!allowed.includes(c)) continue; 
+    if (c === '.' && prevChar === '.') continue; 
 
-    const ALLOWEDCHAR = 'abcdefghijklmnopqrstuvwxyz0123456789-_.';
-    id = [...id].filter(char => ALLOWEDCHAR.includes(char)).join('');
-    
-    let collapsed = '';
-    for (let i = 0; i < id.length; i++) {
-        if (!(id[i] === '.' && collapsed[collapsed.length - 1] === '.')) {
-            collapsed += id[i];
-        }
-    }
-    id = collapsed;
-    
-    if(id[0] === '.') id = id.slice(1);
-    if(id.length >= 16) id = id.slice(0, 15);
-    if(id[id.length - 1] === '.') id = id.slice(0, -1);
-    
-    if (id === '') id = 'a';
-    
-    while(id.length < 3){
-        id += id[id.length -1];
-    }
-    
-    return id;
+    id += c;
+    prevChar = c;
+  }
+
+
+  if (id[0] === '.') id = id.slice(1);
+
+  if (id === '') id = 'a';
+
+  if (id.length >= 16) {
+    id = id.slice(0, 15);
+  }
+  
+  if (id[id.length - 1] === '.') id = id.slice(0, -1);
+
+  while (id.length < 3) {
+    id += id[id.length - 1];
+  }
+
+  return id;
 }
