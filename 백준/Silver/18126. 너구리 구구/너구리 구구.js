030c7d6ff4e88f3d25e54f296fdacb944a8fb23f
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-22 19:02:29 +0900

    [Silver II] Title: 너구리 구구, Time: 188 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Silver/18126. 너구리 구구/너구리 구구.js
+++ 백준/Silver/18126. 너구리 구구/너구리 구구.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
 const N = Number(input[0]);
-const adjacencyList = Array.from({length: N + 1}, () => []);
-const visited = Array(N + 1).fill(false);
+const adjacencyList = Array.from({ length: N + 1 }, () => []);
 
 for (let i = 1; i < input.length; i++) {
   const [from, to, weight] = input[i].split(' ').map(Number);
@@ -12,16 +12,15 @@ for (let i = 1; i < input.length; i++) {
 
 let maxDistance = 0;
 
-function explore(node, distance) {
-  visited[node] = true;
+function explore(curNode, parentNode, distance) {
   maxDistance = Math.max(maxDistance, distance);
-  
-  for (const [nextNode, weight] of adjacencyList[node]) {
-    if (!visited[nextNode]) {
-      explore(nextNode, distance + weight);
+
+  for (const [nextNode, weight] of adjacencyList[curNode]) {
+    if (nextNode !== parentNode) {
+      explore(nextNode, curNode, distance + weight);
     }
   }
 }
 
-explore(1, 0);
+explore(1, 0, 0);
 console.log(maxDistance);
