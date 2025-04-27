57c9298ba05ca85008850aab94c9db8992bcc35d
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-27 22:47:54 +0900

    [Silver V] Title: 브실이의 입시전략, Time: 176 ms, Memory: 16640 KB -BaekjoonHub

--- 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.js
+++ 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.js
@@ -1,36 +1,40 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
 const [N, M, K] = input[0].split(' ').map(Number);
 
 const grades = new Map();
-for(let i = 0; i < N; i++){
-    const [subject, score] = input[i + 1].split(' ');
+for (let i = 1; i <= N; i++) {
+    const [subject, score] = input[i].split(' ');
     grades.set(subject, Number(score));
 }
 
-const subjects = [];
-for(let i = 0; i < K; i++){
-    subjects.push(input[i + N + 1])
+const subjects = new Set();
+for (let i = N + 1; i <= N + K; i++) {
+    subjects.add(input[i]);
 }
 
 let scoreSum = 0;
+for (const subject of subjects) {
+    scoreSum += grades.get(subject) || 0;
+}
 
-for(const sub of subjects){
-    if(grades.has(sub)){
-        scoreSum += grades.get(sub)
+const remainingScores = [];
+for (const [subject, score] of grades) {
+    if (!subjects.has(subject)) {
+        remainingScores.push(score);
     }
 }
 
-const remainingGrades = Array.from(grades)
-    .filter(([subject, _]) => !subjects.includes(subject))
-    .sort((a, b) => b[1] - a[1]);
+remainingScores.sort((a, b) => b - a);
+
 const additional = M - K;
 let maxScore = scoreSum;
 let minScore = scoreSum;
-for(let i = 0; i < additional; i++){
-    maxScore += remainingGrades[i][1];
-    minScore += remainingGrades[remainingGrades.length - 1 - i][1];
+
+for (let i = 0; i < additional; i++) {
+    maxScore += remainingScores[i];
+    minScore += remainingScores[remainingScores.length - 1 - i];
 }
 
-
-console.log(minScore + ' ' + maxScore)
+console.log(`${minScore} ${maxScore}`);
