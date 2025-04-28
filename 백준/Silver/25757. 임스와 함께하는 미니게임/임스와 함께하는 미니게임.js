8717b69bb7c1c45042ffc38335bb88380b345574
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-28 21:11:16 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 144 ms, Memory: 25972 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.js
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.js
@@ -2,15 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [N, game] = input[0].split(' ');
 
-const players = new Set();
-for(let i = 1; i <= Number(N); i++){
-    players.add(input[i]);
-}
+const players = new Set(input.slice(1));
+const divisor = {"Y": 1, "F": 2, "O": 3};
 
-if (game === 'Y') {
-    console.log(players.size);
-} else if (game === 'F') {
-    console.log(Math.floor(players.size / 2));
-} else if (game === 'O') {
-    console.log(Math.floor(players.size / 3));
-}
+console.log(Math.floor(players.size / divisor[game]))
