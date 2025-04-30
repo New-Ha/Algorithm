26506d51a84ff55b9791e4dda11b233551ce1a9f
UHa <141496181+New-Ha@users.noreply.github.com>
2025-04-30 18:04:37 +0900

    [Silver IV] Title: 좋은 단어, Time: 184 ms, Memory: 18928 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.js
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.js
@@ -4,24 +4,14 @@ const N = Number(input[0]);
 const words = input.slice(1);
 
 function isGoodWord(word){
-	const chars = word.split('');
 	const stack = [];
 	
-	for(const char of chars){
+	for(const char of word){
 		const top = stack[stack.length -1];
-		if(top === char) {
-			stack.pop();
-		} else {
-			stack.push(char);
-		}
+		top === char ? stack.pop() : stack.push(char);
 	}
-	
-	return stack.length === 0;
+	return !stack.length;	
 }
 
-let count = 0;
-for(const word of words){
-    if(isGoodWord(word)) count++;
-}
-
+const count = words.filter(isGoodWord).length;
 console.log(count)
