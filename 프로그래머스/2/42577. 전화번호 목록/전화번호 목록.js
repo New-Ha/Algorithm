7c9a17d23737828a43e7b13d0de3b67423e412bc
UHa <141496181+New-Ha@users.noreply.github.com>
2025-05-07 00:02:08 +0900

    [level 2] Title: 전화번호 목록, Time: 154.73 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,20 @@
 function solution(phone_book) {
+    // case 1. sort & startsWith()
     const sorted = phone_book.sort();
     for(let i = 0; i < phone_book.length - 1; i++){
         if(sorted[i+1].startsWith(sorted[i])) return false;
     }
     return true;
+    
+    // case 2. hash
+//     const set = new Set(phone_book);
+
+//     for(const phone of phone_book) {
+//         let prefix = "";
+//         for(let i = 0; i < phone.length - 1; i++) {
+//             prefix += phone[i];
+//             if(set.has(prefix)) return false;
+//         }
+//     }
+//     return true;
 }
