f1e94de7a7b299924480df139bbffa05ac279cac
UHa <141496181+New-Ha@users.noreply.github.com>
2025-05-08 20:24:03 +0900

    [level 2] Title: 구명보트, Time: 12.88 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,19 +1,16 @@
 function solution(people, limit) {
-    const sorted = people.sort((a, b) => a - b);
+    people.sort((a, b) => a - b);
     
     let boat = 0;
     let left = 0;
-    let right = sorted.length - 1;
+    let right = people.length - 1;
     
     while(left <= right){
-        if(sorted[left] + sorted[right] <= limit){
-            boat++;
+        if(people[left] + people[right] <= limit){
             left++;
-            right--;
-        } else {
-            boat++;
-            right--;
         }
+        right--;
+        boat ++;
     }
     
     return boat;
