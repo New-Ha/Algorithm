1954d57da64f33c6e78f7768fb591b3ea711671a
UHa <141496181+New-Ha@users.noreply.github.com>
2025-07-05 22:07:09 +0900

    [Bronze I] Title: 2진수 8진수, Time: 236 ms, Memory: 31904 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.js
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
 
+const targetBinary = input.padStart(Math.ceil(input.length / 3) * 3, '0');
 
-const decimal = BigInt('0b' + input);
-console.log(decimal.toString(8)); 
+let result = '';
+for (let i = 0; i < targetBinary.length; i += 3) {
+    const binNum = targetBinary.slice(i, i + 3);
+    const digitNum = parseInt(binNum, 2);
+    result += digitNum.toString();
+}
+
+console.log(result);
