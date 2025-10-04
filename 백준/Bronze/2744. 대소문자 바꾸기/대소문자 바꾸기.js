62035a5c869a9478011efe8cd25b8785323a1bb2
UHa <141496181+New-Ha@users.noreply.github.com>
2025-10-04 19:56:49 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 92 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.js
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.js
@@ -1,24 +1,10 @@
 const fs = require('fs');
-const str = fs.readFileSync('/dev/stdin').toString().trim().split('');
-const upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const str = fs.readFileSync('/dev/stdin').toString().trim();
 
-const result = str.map(c => {
-    let type = '';
-    let idx = 0;
-    if(upper.indexOf(c) > -1){
-        type = 'lower';
-        idx = upper.indexOf(c);
-    } else {
-        type = 'upper';
-        idx = lower.indexOf(c);
-    }
-    
-    if(type === 'upper'){
-        return upper[idx]
-    }else{
-        return lower[idx]
-    }
-})
+let result = '';
 
-console.log(result.join(''))
+for (const c of str) {
+    result += (c === c.toUpperCase()) ? c.toLowerCase() : c.toUpperCase();
+}
+
+console.log(result);
