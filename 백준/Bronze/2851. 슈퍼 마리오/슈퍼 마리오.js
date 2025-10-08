51fd00a7898e72136fe151b6c64526714564bb9f
UHa <141496181+New-Ha@users.noreply.github.com>
2025-10-08 16:03:41 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.js
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const mushrooms = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
-const score = [mushrooms[0]];
-for(let i = 1; i < mushrooms.length; i++){
-    const cum = score[i - 1] + mushrooms[i];
-    score.push(cum);
-    
-    if(cum >= 100) break;
+
+let sum = 0;
+let prev = 0;
+
+for (const m of mushrooms) {
+  prev = sum;
+  sum += m;
+  if (sum >= 100) break;
 }
 
-const result1 = Math.abs(100 - score[score.length - 1]);
-const result2 = Math.abs(100 - score[score.length - 2]);
-if(result1 > result2){
-    console.log(score[score.length - 2]);
+if (Math.abs(100 - sum) > Math.abs(100 - prev)) {
+  console.log(prev);
 } else {
-    console.log(score[score.length - 1]);
+  console.log(sum);
 }
