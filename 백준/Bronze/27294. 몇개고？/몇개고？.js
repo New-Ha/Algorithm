3eaed6f804bccc1afc1f9ce54f541d072eb41ecc
UHa <141496181+New-Ha@users.noreply.github.com>
2025-12-21 02:51:34 +0900

    [Bronze V] Title: 몇개고?, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.js
+++ 백준/Bronze/27294. 몇개고？/몇개고？.js
@@ -1,8 +1,3 @@
 const fs = require('fs'); 
 const [T, S] = fs.readFileSync(0, 'utf-8').toString().trim().split(' ').map(Number);
-
-if(T >= 12 && T <= 16 && S === 0){
-    console.log(320)
-}else{
-    console.log(280)
-}
+console.log(T >= 12 && T <= 16 && S === 0 ? 320 : 280)
