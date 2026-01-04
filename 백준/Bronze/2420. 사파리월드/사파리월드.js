9ce029c8f08e74220f3c15593e86fc97f62cd6cc
UHa <141496181+New-Ha@users.noreply.github.com>
2026-01-05 02:04:30 +0900

    [Bronze V] Title: 사파리월드, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2420. 사파리월드/사파리월드.js
+++ 백준/Bronze/2420. 사파리월드/사파리월드.js
@@ -1,4 +1,9 @@
 const fs = require('fs'); 
 const nums = fs.readFileSync(0, 'utf-8').toString().trim().split(' ').map(Number);
-nums.sort((a, b) => a - b);
-console.log(nums[1] - nums[0]);
+
+// 1
+// nums.sort((a, b) => a - b);
+// console.log(nums[1] - nums[0]);
+
+// 2
+console.log(Math.abs(nums[0] - nums[1]))
