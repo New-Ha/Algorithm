3a5739ae4cbfb162d6dd5dd6c426198dc5d85bcd
UHa <141496181+New-Ha@users.noreply.github.com>
2026-01-10 01:05:02 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,4 @@
 SELECT O.animal_id, O.name
-FROM animal_ins AS I RIGHT OUTER JOIN animal_outs AS O ON I.animal_id = O.animal_id
-GROUP BY O.animal_id
+FROM animal_outs O LEFT JOIN animal_ins I ON O.animal_id = I.animal_id
 ORDER BY DATEDIFF(O.datetime, I.datetime) DESC
 LIMIT 2;
