a04cfa9c664abd197745886197c524755c40203e
UHa <141496181+New-Ha@users.noreply.github.com>
2026-01-09 21:24:25 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,12 @@
+-- SELECT O.animal_id, O.name
+-- FROM animal_outs O 
+-- LEFT JOIN animal_ins I
+--     ON O.animal_id = I.animal_id
+-- WHERE I.animal_id IS NULL
+-- ORDER BY O.animal_id
+
 SELECT O.animal_id, O.name
-FROM animal_outs O 
-LEFT JOIN animal_ins I
-    ON O.animal_id = I.animal_id
-WHERE I.animal_id IS NULL
-ORDER BY O.animal_id
+FROM animal_outs O, animal_ins I
+WHERE O.animal_id = I.animal_id(+)
+    AND I.animal_id IS NULL
+ORDER BY O.animal_id;
