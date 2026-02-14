b820a8d297d49b1d3c9c12708c9a74a55c3716e3
UHa <141496181+New-Ha@users.noreply.github.com>
2026-02-14 21:52:16 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,8 +1,12 @@
-SELECT user_id, nickname, CONCAT(city, ' ' ,street_address1,' ', street_address2) AS '전체주소', CONCAT(SUBSTR(tlno, 1, 3), '-', SUBSTR(tlno, 4, 4), '-', SUBSTR(tlno, 8)) AS '전화번호' 
-FROM used_goods_user
-WHERE user_id IN (SELECT writer_id
-    FROM used_goods_board
-    GROUP BY writer_id
-    HAVING count(board_id) >= 3)
-ORDER BY user_id DESC
-
+SELECT u.user_id, 
+       u.nickname, 
+       CONCAT(u.city, ' ', u.street_address1, ' ', u.street_address2) AS '전체주소', 
+       CONCAT(SUBSTR(u.tlno, 1, 3), '-', SUBSTR(u.tlno, 4, 4), '-', SUBSTR(u.tlno, 8)) AS '전화번호'
+FROM used_goods_user u
+  JOIN (
+      SELECT writer_id
+      FROM used_goods_board
+      GROUP BY writer_id
+      HAVING COUNT(board_id) >= 3
+  ) b ON u.user_id = b.writer_id
+ORDER BY u.user_id DESC;
