0c9616e7d2e86a5bc4f2b3745481960a78d6ecb3
UHa <141496181+New-Ha@users.noreply.github.com>
2026-02-28 13:08:25 +0900

    [Silver V] Title: 나이순 정렬, Time: 2560 ms, Memory: 50892 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,10 +1,6 @@
 n = int(input())
 
-users = []
-for _ in range(n):
-    age, name = input().split()
-    users.append((int(age), name))
-
+users = [(int(age), name) for age, name in (input().split() for _ in range(n))]
 users.sort(key=lambda x: x[0])
 
 print('\n'.join(f"{age} {name}" for age, name in users))
