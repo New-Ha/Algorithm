c428f50d551154179bab28f1afddd976ed4db50e
UHa <141496181+New-Ha@users.noreply.github.com>
2026-03-02 21:03:50 +0900

    [Silver V] Title: 배열 합치기, Time: 1692 ms, Memory: 431404 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
-const [nm, ...input] = fs.readFileSync(0, 'utf8').trim().split('\n');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number);
+const A = input[1].split(' ').map(Number);
+const B = input[2].split(' ').map(Number);
 
-const mergeArr = [];
-for (const line of input) {
-  mergeArr.push(...line.split(' ').map(Number));
+let i = 0, j = 0;
+const out = [];
+
+while (i < N && j < M) {
+  if (A[i] <= B[j]) out.push(A[i++]);
+  else out.push(B[j++]);
 }
 
-console.log(mergeArr.sort((a, b) => a - b).join(' '));
+while (i < N) out.push(A[i++]);
+while (j < M) out.push(B[j++]);
+
+console.log(out.join(' '));
