1bf74518fd4f5ada3a9aa85e4f59a25e85f50944
UHa <141496181+New-Ha@users.noreply.github.com>
2026-03-06 20:22:16 +0900

    [Silver V] Title: 거스름돈, Time: 96 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.js
+++ 백준/Silver/14916. 거스름돈/거스름돈.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0, 'utf-8').toString().trim());
 
-let count;
-let isPossible = false;
+let count = -1;
 for(o = Math.floor(n / 5); o >= 0; o--){
     let charge = n - o * 5;
     
     if(charge % 2 === 0){
         count = o + charge / 2;
-        isPossible = true;
         break;
     }
 }
 
-console.log(isPossible === true ? count : -1)
+console.log(count)
