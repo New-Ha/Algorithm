2d9c50efca9e5149bbf7c773e388b333c5b61a34
UHa <141496181+New-Ha@users.noreply.github.com>
2026-03-20 22:43:23 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const [a, b, c] = fs.readFileSync(0, 'utf8').trim().split('\n').map(Number);
+
+if(a + b + c !== 180) console.log("Error");
+else if(a === 60 && a === b && a === c) console.log("Equilateral");
+else if(a !== b && a !== c && b !== c) console.log("Scalene");
+else console.log("Isosceles")
